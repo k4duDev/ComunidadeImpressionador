8c761b79e9fd9463fc13b6cce449591246daef89
kadu13 <k4du.abraao@gmail.com>
2024-06-28 09:13:23 -0300

    primeiro commit

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from flask import app
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmacao da Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_subimit_criarconta =SubmitField('Criar conta')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembra-Me')
+    botao_subimit_login =SubmitField('Fazer Login')
